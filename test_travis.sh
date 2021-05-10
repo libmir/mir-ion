85874e2432e949f6cae0c273c44765619d2c7208
ilya <ilyayaroshenko@gmail.com>
2021-05-10 20:20:28 +0700

    fixup

--- test_travis.sh
+++ test_travis.sh
@@ -5,12 +5,3 @@ then
 else
   dub test --arch=$ARCH --build=unittest
 fi
-cd meson-example
-meson build --default-library=static --buildtype=debug
-ninja -C build
-if [ "$TRAVIS_OS_NAME" = "osx" ]
-then
-  gcc test.c build/libion-example.dylib
-else
-  gcc test.c build/libion-example.so
-LD_LIBRARY_PATH=build ./a.out
