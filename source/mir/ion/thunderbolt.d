2f7da2e164290ab7d44e09003bcf5a6cb5cff3a5
ilya <ilyayaroshenko@gmail.com>
2022-06-07 17:24:25 +0400

    ad thunderbolt

--- source/mir/ion/thunderbolt.d
+++ source/mir/ion/thunderbolt.d
@@ -1,6 +1,10 @@
 ///
 module mir.ion.thunderbolt;
 
+import mir.ion.type_code;
+import mir.ion.value: IonDescriptor;
+import core.stdc.string: memmove;
+
 enum ThunderboltStackDepth = 1024;
 
 enum ThunderboltStatus
@@ -12,106 +16,208 @@ enum ThunderboltStatus
 
 static struct ThunderboltStackMember
 {
-    sizediff_t tailLength;
-    size_t valueLength;
+    ubyte* position;
+    size_t length;
+    size_t annotationsLength;
     // null type code encodes end of the value.
     IonTypeCode type;
 
-@safe pure nothrow @nogc:
+@system pure nothrow @nogc:
 
-    this(IonTypeCode type, size_t length)
+    this(IonTypeCode type, size_t length, ubyte* currentPosition, size_t annotationsLength = 0)
     {
+        this.annotationsLength = annotationsLength;
         this.length = length;
-        this.tailLength = length;
-        this.type = lengtypeth;
+        this.position = currentPosition - length + annotationsLength;
+        this.type = type;
     }
 }
 
-pure nothrow @nogc
+// pure
+ubyte[] thunderbolt(const(ubyte)[] joy)
+{
+    import mir.exception: MirException;
+    auto ion = new ubyte[joy.length + 16];
+    if (auto error = thunderbolt(ion.ptr + 16, joy.ptr, joy.length))
+        throw new MirException("thunderbolt: ", error);
+    return ion[16 .. $];
+}
+
+unittest
+{
+    import mir.test;
+    // empty input
+    [].thunderbolt.should == [];
+    // null
+    [0x0F].thunderbolt.should == [0x0F];
+    [0x0F].thunderbolt.should == [0x0F];
+    [0x1F].thunderbolt.should == [0x1F];
+    [0x2F].thunderbolt.should == [0x2F];
+    [0x3F].thunderbolt.should == [0x3F];
+    [0x4F].thunderbolt.should == [0x4F];
+    [0x5F].thunderbolt.should == [0x5F];
+    [0x6F].thunderbolt.should == [0x6F];
+    [0x7F].thunderbolt.should == [0x7F];
+    [0x8F].thunderbolt.should == [0x8F];
+    [0x9F].thunderbolt.should == [0x9F];
+    [0xAF].thunderbolt.should == [0xAF];
+    [0xBF].thunderbolt.should == [0xBF];
+    [0xCF].thunderbolt.should == [0xCF];
+    [0xDF].thunderbolt.should == [0xDF];
+    // zero lengths
+    [0x00].thunderbolt.should == [0x00];
+    [0x10].thunderbolt.should == [0x10];
+    [0x20].thunderbolt.should == [0x20];
+    [0x30].thunderbolt.should == [0x30];
+    [0x40].thunderbolt.should == [0x40];
+    [0x50].thunderbolt.should == [0x50];
+    [0x60].thunderbolt.should == [0x60];
+    [0x70].thunderbolt.should == [0x70];
+    [0x80].thunderbolt.should == [0x80];
+    [0x90].thunderbolt.should == [0x90];
+    [0xA0].thunderbolt.should == [0xA0];
+    [0xB0].thunderbolt.should == [0xB0];
+    [0xC0].thunderbolt.should == [0xC0];
+    [0xD0].thunderbolt.should == [0xD0];
+    // and true
+    [0x11].thunderbolt.should == [0x11];
+
+    // nop
+    [0x33, 0x01].thunderbolt.should == [0x01, 0x33];
+    [0x33, 0x81, 0x0E].thunderbolt.should == [0x0E, 0x81, 0x33];
+    [0x33, 0x34, 0x82, 0x0E].thunderbolt.should == [0x0E, 0x82, 0x33, 0x34];
+    // int
+    [0x03, 0x21].thunderbolt.should == [0x21, 0x03];
+    [0x03, 0x95, 0x82, 0x3E].thunderbolt.should == [0x3E, 0x82, 0x03, 0x95];
+    /// list
+    [0x11, 0xB1].thunderbolt.should == [0xB1, 0x11];
+    [0x10, 0x11, 0xB2].thunderbolt.should == [0xB2, 0x10, 0x11];
+
+    [0x11, 0x21, 0x23, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2D, 0x8F, 0xBE].thunderbolt.should ==
+    [0xBE, 0x8F, 0x11, 0x2D, 0x21, 0x23, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D];
+
+    // struct
+    [0x84, 0x10, 0x85, 0x11, 0x0F, 0xB2, 0xD6].thunderbolt.should ==
+    [0xD6, 0x84, 0x10, 0x85, 0xB2, 0x11, 0x0F];
+
+    // annotation
+    [0x87, 0x88, 0x84, 0x10, 0x85, 0x11, 0x0F, 0xB2, 0x89, 0x11, 0xD8, 0x82, 0xEB].thunderbolt.should ==
+    [0xEB, 0x82, 0x87, 0x88, 0xD8, 0x84, 0x10, 0x85, 0xB2, 0x11, 0x0F, 0x89, 0x11];
+}
+
+    import mir.stdio;
+    import mir.format;
+
+
+// pure nothrow @nogc
 ThunderboltStatus thunderbolt(
            ubyte* ion,
     const(ubyte)* joy,
            size_t length
 )
-    in (destination + 16 <= source  // 16 bytes are enough
-      || source + length <= destination)
+    in (ion + 16     <= joy  // 16 bytes are enough
+     || joy + length <= ion)
 {
-    StackMember[ThunderboltStackDepth + 1] stack = void;
+    ThunderboltStackMember[ThunderboltStackDepth] stack = void;
     size_t stackLength;
 
-    auto current
-        = stack[stackLength++]
-        = ThunderboltStackMember(IonTypeCode.null_, length);
+    debug auto f = tout;
+            debug f.printHexArray!(char, AssumeNothrowFile, ubyte)(joy[0 .. length]);
+            debug tout << endl;
+
+    ion += length;
+    joy += length;
+
+    auto current = ThunderboltStackMember(IonTypeCode.null_, length, ion);
 
     for(;;)
     {
-        if (current.tailLength)
+        debug tout << joy << current << endl;
+        if (current.position < ion)
         {
-            // continue parsing
-        }
-        else
-        if (current..type != IonTypeCode.null_)
-        {
-            auto valueLength = current..length;
-            stackLength--;
-            tailLength = stack[stackLength - 1].tailLength;
+            auto descriptorByte = *--joy;
+            debug tout << descriptorByte.hexAddress << endl;
 
-            if (stackLength)
-        }
-        else
-        {
-            return ThunderboltStatus.success;
-        }
-    }
-}
-
-// assumes 16 bytes end-padding
-ubyte* reverseTapeUnsafe(return ubyte* tape, size_t length)
-    pure nothrow @nogc
-{
-    import mir.ion.value: IonDescriptor;
-    auto s = tape + length - 1;
-    auto d = tape + length - 1 + 16;
-    tape += 16;
-
-
-    //  parseFloating();
-
-    while (tape <= d)
-    {
-        auto b = *s--;
-        if (b < 0x20) // null, bool, 0
-        {
-            *d-- = b;
+            auto descriptor = descriptorByte.IonDescriptor;
+            if (descriptor.type == IonTypeCode.bool_ || descriptor.L == 0 || descriptor.L == 0xF)
+            {
+                *--ion = descriptorByte;
+                if (current.type == IonTypeCode.struct_)
+                    ion.ionPutVarUIntR(joy.parseVarUIntUnsafeR);
+                continue;
+            }
+            if (descriptor.type < IonTypeCode.list)
+            {
+                if (descriptor.L < 0xE)
+                {
+                    // (ion - 16)[0 .. 16] = (joy - 16)[0 .. 16];
+                    (ion - descriptor.L)[0 .. descriptor.L] = (joy - descriptor.L)[0 .. descriptor.L];
+                    ion -= descriptor.L;
+                    joy -= descriptor.L;
+                    *--ion = descriptorByte;
+                    if (current.type == IonTypeCode.struct_)
+                        ion.ionPutVarUIntR(joy.parseVarUIntUnsafeR);
+                    continue;
+                }
+                auto currentLength = joy.parseVarUIntUnsafeR;
+                debug tout << "scalar length = " << currentLength << endl;
+                ion -= currentLength;
+                joy -= currentLength;
+                memmove(ion, joy, currentLength);
+                ion.ionPutVarUIntR(currentLength);
+                *--ion = descriptorByte;
+                if (current.type == IonTypeCode.struct_)
+                    ion.ionPutVarUIntR(joy.parseVarUIntUnsafeR);
+                continue;
+            }
+            stack[stackLength++] = current;
+            size_t currentLength = descriptor.L;
+            if (descriptor.L == 0xE)
+                currentLength = joy.parseVarUIntUnsafeR;
+            if (descriptor.type <= IonTypeCode.struct_)
+            {
+                current = descriptor.type.ThunderboltStackMember(currentLength, ion);
+                continue;
+            }
+            assert(descriptor.type == IonTypeCode.annotations);
+            size_t annotationsLength = joy.parseVarUIntUnsafeR;
+            current = descriptor.type.ThunderboltStackMember(currentLength, ion, annotationsLength);
             continue;
         }
-        auto descriptor = b.IonDescriptor;
-        if (descriptor.type < IonTypeCode.list) // all not structural
+        if (current.type != IonTypeCode.null_)
         {
-            if (descriptor.L < 0xE)
+            assert(stackLength);
+            assert(current.position == ion);
+            if (current.type == IonTypeCode.annotations)
             {
-                *cast(ubyte[8]*) (d - 8 + 1) = *cast(ubyte[8]*) (s - 8 + 1);
+                auto targetIon = current.position - current.annotationsLength;
+                assert(current.annotationsLength);
+                do ion.ionPutVarUIntR(joy.parseVarUIntUnsafeR);
+                while(targetIon < ion);
+                assert(targetIon == ion);
+                ion.ionPutVarUIntR(current.annotationsLength);
             }
-            else
-            if (descriptor.L == 0xE) // typed null values
+            if (current.length < 0xE)
             {
-                auto currentLength = parseVarUIntUnsafeR(s);
-                memmove((d -= currentLength) + 1, (s -= currentLength) + 1, currentLength);
-                ionPutVarUIntR(d, currentLength);
+                *--ion = cast(ubyte) ((current.type << 4) | current.length);
+                current = stack[--stackLength];
+                if (current.type == IonTypeCode.struct_)
+                    ion.ionPutVarUIntR(joy.parseVarUIntUnsafeR);
+                continue;
             }
+            ion.ionPutVarUIntR(current.length);
+            *--ion = cast(ubyte) ((current.type << 4) | 0xE);
+            current = stack[--stackLength];
+            if (current.type == IonTypeCode.struct_)
+                ion.ionPutVarUIntR(joy.parseVarUIntUnsafeR);
+            continue;
         }
-        else
-        {
-            if (descriptor.L == 0)
-                continue;
-            if (descriptor.L > 0xE) // typed null values
-                continue;
-            size_t currentLength = descriptor.L < 0xE ? descriptor.L : parseVarUIntUnsafeR(s);
-        }
+        assert(current.position <= ion);
+        assert(current.position >= ion);
+        return ThunderboltStatus.success;
     }
-    assert(d + 1 == tape);
-    return tape;
 }
+
 
 package size_t parseVarUIntUnsafeR(ref inout(ubyte)* s)
     pure nothrow @nogc
@@ -120,10 +226,11 @@ package size_t parseVarUIntUnsafeR(ref inout(ubyte)* s)
     version (LDC) pragma(inline, true);
     for(;;)
     {
-        ubyte b = *s--;
+        byte b = *--s;
         result <<= 7;
         result |= b & 0x7F;
-        if (cast(byte)b < 0)
+        debug tout << "  b " << ubyte(b).hexAddress << " temp var len = " << result << endl;
+        if (b < 0)
             return result;
     }
 }
@@ -131,7 +238,7 @@ package size_t parseVarUIntUnsafeR(ref inout(ubyte)* s)
 private void ionPutVarUIntR(ref ubyte* ptr, size_t value)
     pure nothrow @nogc
 {
-    do *ptr-- = value & 0x7F;
+    do *--ptr = value & 0x7F;
     while (value >>>= 7);
-    ptr[1] |= 0x80;
+    *ptr |= 0x80;
 }
